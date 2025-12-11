780fedc0ec66c674f5f91aaf5af4af2faacf4409
shanon082 <shanonsimon082@gmail.com>
2025-12-11 18:37:44 +0300

    Added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:saidia_app/firebase_options.dart';
 import 'package:saidia_app/splashscreen.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
 }
 
